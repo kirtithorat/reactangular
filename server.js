49fedbee452bb40119032da7563886631c6946ff
kirtithorat <kirti.brenz@gmail.com>
2015-03-24 14:36:46 -0500

    Introduce Angular in the app

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+app.use("/bower_components", express.static('bower_components'));
 
 module.exports = app;
