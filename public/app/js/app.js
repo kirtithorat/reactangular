57d20571b8b5419de6bd4ee54bb6856cc79f3c94
kirtithorat <kirti.brenz@gmail.com>
2015-04-04 22:21:16 -0500

    Add Watchers Stats

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,9 +1,10 @@
 angular.module("reactangularApp", ['ngResource'])
 .controller('AppCtrl', [ '$scope', 'ProductsFactory', '$timeout', function AppCtrl($scope, ProductsFactory, $timeout) {
 	$scope.getProducts = function () {
+		$scope.clearProducts();
 		ProductsFactory.query(function (products) {
 			$scope.products = products;
-			$scope.message = "Got "+products.length+ " products";
+			$scope.message = "Products Count: "+products.length;
 		});
 
 		$scope.message = "Fetching Products..."
@@ -23,10 +24,45 @@ angular.module("reactangularApp", ['ngResource'])
 		$scope.message = "";
 		$scope.showAngular = false;
 		$scope.showAngularReact = false;
+		$scope.watchersCount = 0;
 	};
 	$scope.setTimer = function(timer) {
 		$scope.timer = timer;
 	};
+	$scope.countWatchers = function () {
+        (function () { 
+		    var root = angular.element(document.getElementsByTagName('body'));
+
+		    var watchers = [];
+
+		    var f = function (element) {
+		        angular.forEach(['$scope', '$isolateScope'], function (scopeProperty) { 
+		            if (element.data() && element.data().hasOwnProperty(scopeProperty)) {
+		                angular.forEach(element.data()[scopeProperty].$$watchers, function (watcher) {
+		                    watchers.push(watcher);
+		                });
+		            }
+		        });
+
+		        angular.forEach(element.children(), function (childElement) {
+		            f(angular.element(childElement));
+		        });
+		    };
+
+		    f(root);
+
+		    // Remove duplicate watchers
+		    var watchersWithoutDuplicates = [];
+		    angular.forEach(watchers, function(item) {
+		        if(watchersWithoutDuplicates.indexOf(item) < 0) {
+		             watchersWithoutDuplicates.push(item);
+		        }
+		    });
+
+		    $scope.watchersCount = watchersWithoutDuplicates.length;
+		})();
+    };
+
 	function renderingTime() {
         var startTime = new Date().getTime();
         $timeout(function () {
@@ -34,6 +70,7 @@ angular.module("reactangularApp", ['ngResource'])
             $scope.setTimer(time);
         });
     };
+
     $scope.clearProducts();
 }])
 .directive('productsData', [function () {
