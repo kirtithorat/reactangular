0005f699d2dfa6586371ae5ecc87dd80befed6ed
kirtithorat <kirti.brenz@gmail.com>
2015-04-04 21:47:53 -0500

    Add Benchmarking to Angular Rendering

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,10 +1,40 @@
 angular.module("reactangularApp", ['ngResource'])
-.controller('AppCtrl', [ '$scope', 'ProductsFactory', function AppCtrl($scope, ProductsFactory) {
-	$scope.getProductsData = function () {
+.controller('AppCtrl', [ '$scope', 'ProductsFactory', '$timeout', function AppCtrl($scope, ProductsFactory, $timeout) {
+	$scope.getProducts = function () {
 		ProductsFactory.query(function (products) {
-			$scope.productsData = products;
+			$scope.products = products;
+			$scope.message = "Got "+products.length+ " products";
 		});
+
+		$scope.message = "Fetching Products..."
 	};
+	$scope.renderAngular = function () {
+		$scope.showAngularReact = false;
+		$scope.showAngular = true;
+		renderingTime();
+	};
+	$scope.renderAngularReact = function () {
+		$scope.showAngular = false;
+		$scope.showAngularReact = true;
+	};
+	$scope.clearProducts = function () {
+		$scope.products = {};
+		$scope.timer = "--";
+		$scope.message = "";
+		$scope.showAngular = false;
+		$scope.showAngularReact = false;
+	};
+	$scope.setTimer = function(timer) {
+		$scope.timer = timer;
+	};
+	function renderingTime() {
+        var startTime = new Date().getTime();
+        $timeout(function () {
+            var time = (new Date().getTime() - startTime) + " ms";
+            $scope.setTimer(time);
+        });
+    };
+    $scope.clearProducts();
 }])
 .directive('productsData', [function () {
 	return {
